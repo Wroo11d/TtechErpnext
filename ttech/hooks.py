fec0270a710242fed696fb1626dc64bae960a49c
Wroo11d <worood.it@gmail.com>
2023-11-06 15:23:43 +0300

    FirstCommit

--- ttech/hooks.py
+++ ttech/hooks.py
@@ -11,7 +11,7 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/ttech/css/ttech.css"
+app_include_css = "/assets/ttech/css/ttech.css"
 # app_include_js = "/assets/ttech/js/ttech.js"
 
 # include js, css files in header of web template
