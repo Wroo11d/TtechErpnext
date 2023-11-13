944b17cc12c618326f4a8b994635de30b3bd3923
Hardik Gadesha <hardik@goldsetu.co>
2023-11-13 12:08:56 +0000

    TTEC-4

--- ttech/ttech/doctype/ttech_settings/ttech_settings.js
+++ ttech/ttech/doctype/ttech_settings/ttech_settings.js
@@ -2,10 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('TTech Settings', {
-	refresh: function(frm) {
-		frm.set_intro('Exchange Rate Updates Automaticaly Daily at 06:00 AM, Only System Manager can Manually Update it')
-		if(frappe.user.has_role('System Manager')){
-			frm.set_df_property('exchange_rate',  'read_only', 0);
-		}
-	}
+	// refresh: function(frm) {
+
+	// }
 });
