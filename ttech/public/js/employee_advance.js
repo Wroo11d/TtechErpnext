1845068177acf5846ba2cb7d170d5825e254c6df
Hardik Gadesha <hardik@goldsetu.co>
2023-11-21 07:02:55 +0000

    TTEC-9

--- ttech/public/js/employee_advance.js
+++ ttech/public/js/employee_advance.js
@@ -3,7 +3,9 @@
 
 frappe.ui.form.on('Employee Advance', {
 	refresh: function(frm) {
-		frm.set_value('creator_id', frappe.session.user);
+		if(frm.doc.__islocal){
+			frm.set_value('creator_id', frappe.session.user);
+		}
 		frm.set_query('currency', function(doc) {
 			return {
 				filters: {
@@ -11,5 +13,19 @@ frappe.ui.form.on('Employee Advance', {
 				}
 			}
 		})
+		make_finance_field_reqd(frm);
 	}
 });
+
+function make_finance_field_reqd(frm) {
+	if(frm.doc.__islocal){
+		frm.set_df_property('advance_account', 'reqd', 0);
+		frm.set_df_property('cost_center', 'reqd', 0);
+	}
+	if(!frm.doc.__islocal && (!frm.doc.advance_account || !frm.doc.cost_center) && 
+	(frappe.user.has_role("CFO") || frappe.user.has_role("Finance Manager") || frappe.user.has_role("Finance Officer"))){
+		frm.dirty();
+		frm.set_df_property('advance_account', 'reqd', 1);
+		frm.set_df_property('cost_center', 'reqd', 1);
+	}
+}
